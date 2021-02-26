9f98d7ab07919083fb76e7ba2365b06e625ffe79
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-02-26 20:55:23 -0300

    Serving Products Back End Routes

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('Api is running')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(p => p._id === req.params.id)
+    res.json(product)
+})
+
+
+const port = 5000
+
+app.listen(port, console.log(`Server Running on ${5000}`))
