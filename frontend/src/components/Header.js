8cfe9d03198b60198e5456f17f01de68e61062a8
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-02-26 20:29:55 -0300

    Product Details Screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,17 +7,23 @@ const Header = () => {
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
-                    <LinkContainer>
-                        <Navbar.Brand to="/">ProShop</Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>ProShop</Navbar.Brand>
                     </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
                             <LinkContainer to="/cart">
-                                <Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                                <Nav.Link>
+                                    <i className="fas fa-shopping-cart"></i>
+                                    Cart
+                                    </Nav.Link>
                             </LinkContainer>
                             <LinkContainer to="/login">
-                                <Nav.Link><i className="fas fa-user"></i> Login</Nav.Link>
+                                <Nav.Link>
+                                    <i className="fas fa-user"></i>
+                                    Login
+                                </Nav.Link>
                             </LinkContainer>
 
                         </Nav>
