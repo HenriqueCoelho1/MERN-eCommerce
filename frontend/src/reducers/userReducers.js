fa648184edf12aa70cecd6e2cda9c4159e7c89a5
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-03-05 22:52:04 -0300

    Profile Screen & Get User Details

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -5,7 +5,10 @@ import {
     USER_LOGOUT,
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
-    USER_REGISTER_FAIL
+    USER_REGISTER_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL
 } from '../actions/types'
 
 export const userLoginReducer = (state = {}, action) => {
@@ -36,6 +39,20 @@ export const userRegisterReducer = (state = {}, action) => {
             return { loading: false, error: payload }
         case USER_LOGOUT:
             return {}
+        default:
+            return state
+    }
+}
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+    const { type, payload } = action
+    switch (type) {
+        case USER_DETAILS_REQUEST:
+            return { ...state, loading: true }
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: payload }
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: payload }
         default:
             return state
     }
