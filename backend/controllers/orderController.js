2b7b33ff8bf6612c6d2c9478c65559b46dd40b1d
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-03-14 15:41:36 -0300

    Show Orders On Profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -82,6 +82,19 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 
 })
 
-export { addOrderItems, getOrderById, updateOrderToPaid }
+
+// @desc    Get logged in user orders
+// @route   GET /api/orders/myorders
+// @access  Private
+const getMyOrders = asyncHandler(async (req, res) => {
+    const order = await Order.find({ user: req.user._id })
+
+
+    res.json(order)
+
+
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
 
 
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -8,7 +8,10 @@ import {
     ORDER_PAY_RESET,
     ORDER_PAY_FAIL,
     ORDER_PAY_SUCCESS,
-    ORDER_PAY_REQUEST
+    ORDER_PAY_REQUEST,
+    ORDER_LIST_MY_REQUEST,
+    ORDER_LIST_MY_SUCCESS,
+    ORDER_LIST_MY_FAIL
 } from '../actions/types'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -86,6 +89,32 @@ export const orderPayReducer = (state = {}, action) => {
             }
         case ORDER_PAY_RESET:
             return {}
+        default:
+            return state
+    }
+
+}
+
+export const orderListMyReducer = (state = { orders: [] }, action) => {
+
+    const { type, payload } = action
+
+    switch (type) {
+        case ORDER_LIST_MY_REQUEST:
+            return {
+                loading: true,
+
+            }
+        case ORDER_LIST_MY_SUCCESS:
+            return {
+                loading: false,
+                orders: payload
+            }
+        case ORDER_LIST_MY_FAIL:
+            return {
+                loading: false,
+                error: payload
+            }
         default:
             return state
     }
