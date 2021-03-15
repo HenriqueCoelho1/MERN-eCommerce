9c628bc9ad9d1292d56404dad4d78e71b29145f4
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-03-15 20:14:54 -0300

    Admin User Delete

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -18,6 +18,9 @@ import {
     USER_LIST_SUCCESS,
     USER_LIST_FAIL,
     USER_LIST_RESET,
+    USER_DELETE_REQUEST,
+    USER_DELETE_SUCCESS,
+    USER_DELETE_FAIL,
 } from '../actions/types'
 
 export const userLoginReducer = (state = {}, action) => {
@@ -96,6 +99,20 @@ export const userListReducer = (state = { users: [] }, action) => {
             return { loading: false, error: payload }
         case USER_LIST_RESET:
             return {}
+        default:
+            return state
+    }
+}
+
+export const userDeleteReducer = (state = {}, action) => {
+    const { type, payload } = action
+    switch (type) {
+        case USER_DELETE_REQUEST:
+            return { loading: true }
+        case USER_DELETE_SUCCESS:
+            return { loading: false, success: true }
+        case USER_DELETE_FAIL:
+            return { loading: false, error: payload }
         default:
             return state
     }
