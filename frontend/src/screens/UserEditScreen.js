fff49c7dc9584fb3df2234fa858397b5bd056051
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-03-16 23:56:42 -0300

    Update User Functionality

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -5,8 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { getUserDetails } from '../actions/userActions'
-
+import { getUserDetails, updateUser } from '../actions/userActions'
+import { USER_UPDATE_RESET } from '../actions/types'
 const UserEditScreen = ({ match, history }) => {
     const userId = match.params.id
 
@@ -18,23 +18,33 @@ const UserEditScreen = ({ match, history }) => {
     const dispatch = useDispatch()
 
     const userDetails = useSelector(state => state.userDetails)
-
     const { loading, error, user } = userDetails
 
+    const userUpdate = useSelector(state => state.userUpdate)
+    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = userUpdate
+
     useEffect(() => {
-        if (!user.name || user._id !== userId) {
-            dispatch(getUserDetails(userId))
-        }
-        else {
-            setName(user.name)
-            setEmail(user.email)
-            setIsAdmin(user.isAdmin)
+        if (successUpdate) {
+            dispatch({ type: USER_UPDATE_RESET })
+            history.push('/admin/userlist')
+        } else {
+            if (!user.name || user._id !== userId) {
+                dispatch(getUserDetails(userId))
+            }
+            else {
+                setName(user.name)
+                setEmail(user.email)
+                setIsAdmin(user.isAdmin)
+            }
+
         }
 
-    }, [dispatch, userId, user])
+
+    }, [dispatch, history, userId, user, successUpdate])
 
     const submitHandler = (e) => {
         e.preventDefault()
+        dispatch(updateUser({ _id: userId, name, email, isAdmin }))
     }
     return (
         <>
@@ -42,10 +52,12 @@ const UserEditScreen = ({ match, history }) => {
 
             <FormContainer>
                 <h1>Update User</h1>
+                {loadingUpdate && <Loader />}
+                {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
                 {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
                     <Form onSubmit={submitHandler}>
                         <Form.Group controlId='name'>
-                            <Form.Label>name Address: </Form.Label>
+                            <Form.Label>Name: </Form.Label>
                             <Form.Control type='name'
                                 placeholder='Enter your name'
                                 value={name}
@@ -67,7 +79,6 @@ const UserEditScreen = ({ match, history }) => {
                             <Form.Check
                                 type='checkbox'
                                 label='Is Admin'
-                                placeholder='Enter isAdmin'
                                 checked={isAdmin}
                                 onChange={(e) => setIsAdmin(e.target.checked)} >
                             </Form.Check>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,8 +3,21 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer } from './reducers/userReducers'
-import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer } from './reducers/orderReducers'
+import {
+    userLoginReducer,
+    userRegisterReducer,
+    userDetailsReducer,
+    userUpdateProfileReducer,
+    userListReducer,
+    userDeleteReducer,
+    userUpdateReducer
+} from './reducers/userReducers'
+import {
+    orderCreateReducer,
+    orderDetailsReducer,
+    orderPayReducer,
+    orderListMyReducer
+} from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -19,7 +32,8 @@ const reducer = combineReducers({
     orderPay: orderPayReducer,
     orderListMy: orderListMyReducer,
     userList: userListReducer,
-    userDelete: userDeleteReducer
+    userDelete: userDeleteReducer,
+    userUpdate: userUpdateReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
