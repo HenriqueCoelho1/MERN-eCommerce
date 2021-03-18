198095bc4c8795f23f2537325416605742732c05
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-03-18 20:48:29 -0300

    Admin Delete Products

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,7 +5,11 @@ import {
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAIL
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    ORDER_LIST_MY_FAIL,
+    PRODUCT_DELETE_FAIL
 } from './types'
 
 
@@ -49,6 +53,40 @@ export const listProductDetails = (id) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_DETAILS_FAIL,
+            payload: error.response && error.response.data.message ?
+                error.response.data.message
+                : error.message
+        })
+
+    }
+
+}
+
+export const deleteProduct = (id) => async (dispatch, getState) => {
+
+    try {
+        dispatch({
+            type: PRODUCT_DELETE_REQUEST,
+        })
+
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        await axios.delete(`/api/products/${id}`, config)
+
+        dispatch({
+            type: PRODUCT_DELETE_SUCCESS,
+        })
+
+    } catch (error) {
+
+        dispatch({
+            type: PRODUCT_DELETE_FAIL,
             payload: error.response && error.response.data.message ?
                 error.response.data.message
                 : error.message
