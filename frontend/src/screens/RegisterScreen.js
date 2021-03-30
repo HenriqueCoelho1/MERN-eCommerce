cfac398e63fe6ddec588d2f23bb664b9671937ff
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-03-30 03:05:39 -0300

    Arrumando

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -83,7 +83,7 @@ const RegisterScreen = ({ location, history }) => {
                 </Form.Group>
 
                 <Button type='submit' variant='primary'>
-                    Register
+                    Registre
                 </Button>
             </Form>
 
